9c07740e413464573517503dee895d135a4ee614
bradleypregon <bradley.pregon@gmail.com>
2024-01-04 18:01:21 -0600

    first commit

---
+++ Client.py
@@ -0,0 +1,27 @@
+import socket
+import time
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+addr = ("127.0.0.1", 49002)
+
+# alternate between coordinates
+
+#41.13, -92.74
+#41.98, -97.30
+#41.44, -115.27
+
+# long,lat,elevation,headingspeed
+while True:
+  sock.sendto(b'XGPSFlight Events,-92.74,41.13,100.0,270,350.0', addr)
+  time.sleep(0.5)
+  sock.sendto(b'XGPSFlight Events,-97.30,41.98,100.0,265,350.0', addr)
+  time.sleep(0.5)
+  sock.sendto(b'XGPSFlight Events,-115.27,41.44,100.0,275,350.0', addr)
+  time.sleep(0.5)
+  sock.sendto(b'XGPSFlight Events,-115.27,41.44,100.0,90,350.0', addr)
+  time.sleep(0.5)
+  sock.sendto(b'XGPSFlight Events,-97.30,41.98,100.0,95,350.0', addr)
+  time.sleep(0.5)
+  sock.sendto(b'XGPSFlight Events,-92.74,41.13,100.0,85,350.0', addr)
+  time.sleep(0.5)
